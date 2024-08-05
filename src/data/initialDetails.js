eb279efc06f1fca5ec9139a2bd3c3e84975419da
Arjun Nourmansyah Ramdani <arjunnourmansyah27@gmail.com>
2024-08-05 15:13:49 +0700

    Arjun ubah

---
+++ src/data/initialDetails.js
@@ -0,0 +1,39 @@
+const initialDetails = [
+  {
+    id: 1,
+    imgPath: "/assets/img/1.png",
+    name: "Zama Arga",
+    email: "argatama@gmail.com",
+    address: "Limo, Depok",
+  },
+  {
+    id: 2,
+    imgPath: "/assets/img/2.png",
+    name: "Arjun",
+    email: "arjun@gmail.com",
+    address: "Cimanggis, Depok",
+  },
+  {
+    id: 3,
+    imgPath: "/assets/img/3.png",
+    name: "Daniel",
+    email: "daniel@gmail.com",
+    address: "Baker Street, India",
+  },
+  {
+    id: 4,
+    imgPath: "/assets/img/4.png",
+    name: "John Holmes",
+    email: "mary@johnlock.com",
+    address: "Baker Street, India",
+  },
+  {
+    id: 5,
+    imgPath: "/assets/img/5.png",
+    name: "Mycroft Lestrade",
+    email: "britishgovt@gmail.com",
+    address: "London, India",
+  },
+];
+
+export default initialDetails;
